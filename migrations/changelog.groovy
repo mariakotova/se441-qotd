cc7222d30e1b05eadf5c1554c081f2d090b985c9
mariakotova <0365kotovam@gmail.com>
2016-06-06 22:50:54 -0500

    Add files via upload

---
+++ migrations/changelog.groovy
@@ -0,0 +1,19 @@
+databaseChangeLog = {
+
+    changeSet(author: "mkotova (generated)", id: "1465270007007-1") {
+        createTable(tableName: "QUOTE") {
+            column(autoIncrement: "true", name: "ID", type: "BIGINT(19)") {
+                constraints(primaryKey: "true", primaryKeyName: "QUOTEPK")
+            }
+
+            column(name: "VERSION", type: "BIGINT(19)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "TEXT", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+    include file: 'week-5A.groovy'
+}
